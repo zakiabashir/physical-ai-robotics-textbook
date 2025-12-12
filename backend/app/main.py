ab8372af0c9464edea257d6079a237daf418fca1
zakiabashir <nshafeys0@gmail.com>
2025-12-12 15:04:35 +0500

    push my code

--- backend/app/main.py
+++ backend/app/main.py
@@ -262,7 +262,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=int(os.getenv("PORT", 8000)),
         reload=settings.DEBUG,
         log_level=settings.LOG_LEVEL.lower(),
     )
